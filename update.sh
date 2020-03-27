a1307473b4ed3aa395eaa2175a4768d61194c739
hppRC <hpp.ricecake@gmail.com>
2020-03-27 23:21:53 +0900

    :tada: initial commit

---
+++ update.sh
@@ -0,0 +1,21 @@
+yarn add -D \
+@babel/core \
+@babel/plugin-syntax-dynamic-import \
+@babel/plugin-transform-runtime \
+@babel/preset-env \
+@babel/runtime \
+@pyoner/svelte-ts-preprocess \
+@rollup/plugin-commonjs \
+@rollup/plugin-json \
+@rollup/plugin-node-resolve \
+@types/node \
+@wessberg/rollup-plugin-ts \
+npm-run-all \
+rollup \
+rollup-plugin-babel \
+rollup-plugin-replace \
+rollup-plugin-svelte \
+rollup-plugin-terser \
+sapper \
+svelte \
+typescript
