050c45f71981feb90ab4b8e7778943bb951fe7a8
hppRC <hpp.ricecake@gmail.com>
2020-04-08 00:30:42 +0900

    :sparkles: merge

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const { resolve } = require(`path`);
 const sitemap = require(`nextjs-sitemap-generator`);
+const withPlugins = require(`next-compose-plugins`);
+const optimizedImages = require(`next-optimized-images`);
 
 sitemap({
   baseUrl: ``,
@@ -7,10 +9,11 @@ sitemap({
   targetDirectory: `public/`,
 });
 
-module.exports = {
+module.exports = withPlugins([[optimizedImages]], {
   target: `serverless`,
   webpack: (config) => {
-    config.resolve.alias[`~`] = resolve(__dirname, `src`);
+    config.resolve.alias.src = resolve(__dirname, `src`);
+    config.resolve.alias.assets = resolve(__dirname, `assets`);
     return config;
   },
-};
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,68 @@ import styled from '@emotion/styled';
 type ContainerProps = {};
 type Props = {} & ContainerProps;
 
-const Component: React.FCX<Props> = ({ className }) => <div className={className}>test</div>;
+const desktopWebp = require(`assets/Desktop.png?webp`);
+const desktopPng = require(`assets/Desktop.png`);
+const ipadWebp = require(`assets/iPadPro11.png?webp`);
+const ipadPng = require(`assets/iPadPro11.png`);
+const iphoneWebp = require(`assets/iPhoneX.png?webp`);
+const iphonePng = require(`assets/iPhoneX.png`);
+
+const Component: React.FCX<Props> = ({ className }) => (
+  <main className={className}>
+    <picture id='desktop'>
+      <source srcSet={desktopWebp} type='image/webp' />
+      <source srcSet={desktopPng} type='image/png' />
+      <img src={desktopPng} alt='test' />
+    </picture>
+    <picture id='ipad'>
+      <source srcSet={ipadWebp} type='image/webp' />
+      <source srcSet={ipadPng} type='image/png' />
+      <img src={ipadPng} alt='test' />
+    </picture>
+    <picture id='iphone'>
+      <source srcSet={iphoneWebp} type='image/webp' />
+      <source srcSet={iphonePng} type='image/png' />
+      <img src={iphonePng} alt='test' />
+    </picture>
+  </main>
+);
 
 const StyledComponent = styled(Component)`
+  #desktop {
+    img {
+      width: 100%;
+    }
+  }
+  #ipad {
+    display: none;
+  }
+  #iphone {
+    display: none;
+  }
   @media screen and (max-width: 1100px) {
+    #desktop {
+      display: none;
+    }
+    #ipad {
+      display: block;
+      img {
+        width: 100%;
+      }
+    }
   }
   @media screen and (max-width: 768px) {
   }
   @media screen and (max-width: 480px) {
+    #ipad {
+      display: none;
+    }
+    #iphone {
+      display: block;
+      img {
+        height: 100%;
+      }
+    }
   }
   @media screen and (max-height: 430px) {
   }
@@ -20,5 +74,4 @@ const StyledComponent = styled(Component)`
 
 const Container: React.FCX<ContainerProps> = (props) => <StyledComponent {...props} />;
 
-export const config = { amp: true };
 export default Container;
